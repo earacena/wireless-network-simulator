17a3b29f22a81bfe2b5502400324794d7fb47602
earacena <30157858+earacena@users.noreply.github.com>
2019-11-14 20:50:14 -0500

    Added DataCollector class to handle collection data during simulation. Added implementations for methods in DataCollector. Added preliminary Python script that will be used to turn simulator data into graphs.

---
+++ DataCollector.h
@@ -0,0 +1,59 @@
+//
+// Project Name: Wireless Network Simulaor
+// File: DataCollector.hpp
+// File Description: This file contains the interface for the DataCollector 
+//                   class and methods.
+//
+
+#ifndef DATACOLLECTOR_HPP
+#define DATACOLLECTOR_HPP
+
+#include <string>
+#include <pair>
+#include <fstream>
+#include "Grid.h"
+
+// DataCollector will record important performance metrics during simulation
+// that will be used to generate graphs and visualizations.
+class DataCollector {
+public:
+
+  DataCollector(const std::string & filename);
+  void initialize(int grid_size, int num_of_channels, int num_of_devices,
+                  const std::vector<std::pair<int, int> > & node_pos,
+                  const std::vector<std::pair<int, int> > & station_pos);
+
+  /* Data collection */
+  // Store computed path as a Path object in a vector.
+  // post: Path object is stored in DataCollector.nodePaths.
+  // void storePath();
+
+  // After finishing a path computation, update the vector containing hops across
+  // n transmissions by recording the value.
+  void update_hops_for_session(int hops);
+
+  // After finishing a path computation, update the vector containing channel switches
+  // across n transmissions by recording the value.
+  void update_switches_for_session(int switches);
+
+  // Write all collected data to file.
+  void export_data();
+
+  // Fill these directly from Grid class.
+  std::vector<std::pair<int, int> > node_positions;
+  std::vector<std::pair<int, int> > station_positions;
+
+private:
+  std::string filename_;
+
+  // Simulator session parameters
+  int grid_size_;
+  int num_of_channels_;
+  int num_of_devices_;
+
+  // Performance metrics
+  std::vector<int> num_of_hops_;
+  int num_of_channel_switches_;
+};
+
+#endif // DATACOLLECTOR_HPP
