a5e30597f36abcdd9d76ee006ea1f537cc189b46
earacena <30157858+earacena@users.noreply.github.com>
2019-12-01 20:21:56 -0500

    Added function for generating "number of channels switches vs. number of channels" graphs.

--- Metrics.py
+++ Metrics.py
@@ -53,6 +53,43 @@ class Metrics:
         plt.title("Number of Hops vs. Number of Nodes")
 
         plt.show()
+
+    def generate_switches_vs_channels(self):
+        print("[METRICS] Generating graph 'switches vs channels' with dataset {}.".format(self.parsed))
+
+        num_of_channels = []
+        for data in self.parsed:
+            print("{}".format(data))
+            num_of_channels.append(data[4])
+
+        num_of_switches = []
+        for data in self.parsed:
+            num_of_switches.append(data[5])
+
+        # Find largest switches count to create the labels on left (switches)
+        largest_switches = 0
+        for switches in num_of_switches:
+            if switches > largest_switches:
+                largest_switches = switches
+
+        # Find largest channel count to create the labels on bottom (channels)
+        largest_channels = 0
+        for channels in num_of_channels:
+            if channels > largest_channels:
+                largest_channels = channels
+
+
+        print("[Metrics] Switches: {}".format(num_of_switches))
+        print("[Metrics] Channels: {}".format(num_of_channels))
+        y_pos = np.arange(len(num_of_channels))        
+        plt.bar(y_pos, num_of_switches, align='center', alpha=0.5)
+        plt.xticks(y_pos, num_of_channels)
+        plt.xlabel("Number of Channels")
+        plt.ylabel("Number of Switches")
+        plt.title("Number of Switches vs. Number of Channels")
+
+        plt.show()
+  
  
 def parse_incoming_data(filename):
     """
@@ -89,6 +126,6 @@ def main():
     metrics = Metrics(filename)
 
     # Generate plots
-    metrics.generate_hops_vs_devices()
-
+    #metrics.generate_hops_vs_devices()
+    metrics.generate_switches_vs_channels()
 main()
