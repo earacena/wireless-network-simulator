6231f2e52e60e4a8a9d755bece62ff70a9066d0a
earacena <30157858+earacena@users.noreply.github.com>
2019-12-08 14:14:39 -0500

    Added implementation of Sender parser using specified format and expected usage of containers.

--- Sender.cpp
+++ Sender.cpp
@@ -2,21 +2,32 @@
 // Project Name: Wireless Network Simulaor
 // File: Sender.cpp
 // File Description: This file contains the implementations for the Sender class.
-
+//                   Assumed data results format:
+//                     typedef std::tuple<std::string, int, std::string> Hop;
+//                     std::vector< std::vector<Hop> >;
 #include "Sender.h"
 
-Sender::Sender() {  }
+Sender::Sender(const std::string & filename) : filename_(filename) {}
 
-void Sender::read_data() { }
-
-void Sender::export_data() {
-  std::string filename = "routes.txt";
-
-  std::fstream file(filename);
-  if (s.is_open()) {
-    // s << data;
-  } else {
-    std::cout << "[ERROR] Failed to open " + filename + "...";
-  }
+void Sender::export_data(const std::vector<std::vector<Hop>> & results) {
+  std::ofstream file(filename_);
+  std::string srcId = "";
+  int channel = 0;
+  std::string destId = "";
   
+  if (file.is_open()) {
+    for (std::vector<Hop> path : results) {
+      for (Hop hop : path) {
+        srcId = std::get<0>(hop);
+        channel = std::get<1>(hop);
+        destId = std::get<2>(hop);
+
+        file << srcId << "," << channel << "," << destId << ",";
+      }
+      file << "\n";
+    }
+    file.close();
+  } else {
+    std::cout << "[ERROR] Failed to open [" + filename_ + "]...";
+  } 
 }
