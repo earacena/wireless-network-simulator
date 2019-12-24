486f901489708216116fdc2024e604f018c6884b
srahim457 <srahim457@gmail.com>
2019-12-24 16:14:12 -0500

    added BFS
    take a look at the if statement structure in the main function. THIS HAS NOT BEEN TESTED YET.

--- NetworkSim.cpp
+++ NetworkSim.cpp
@@ -2,6 +2,7 @@
 #include <utility>
 #include <string>
 #include <vector>
+#include <queue> 
 
 #include "Node.h"
 #include "BaseStation.h"
@@ -81,6 +82,111 @@ void populateBaseStations(vector<BaseStation> &basestations,vector<Node> &nodes,
 }
 void export_data();
 
+//passing the src node and the list of all nodes which is our graph
+vector<string> BFS( vector<Node>& graph, string src_node_name, string dest_node_name){
+	//the vector that holds the route backwards; dest -> src. Basically a stack
+	//I swap it at the end
+	vector<string> backward_route;
+//------------------------------------error handling--------------------------------
+	//find index of the source node
+	int src_index = -1;
+	for(int i = 0; i < graph.size(); i++ ){
+		if( graph[i].getName() == src_node_name){
+			src_index = i;
+		}
+	}
+	//check if the node is even in the graph
+	if (src_index == -1){
+		return backward_route; //node is not in the graph; returns an empty vector
+	}
+	
+	//find the index of the destination node
+	int dest_index = -1;
+	for(int i = 0; i < graph.size(); i++ ){
+		if( graph[i].getName() == dest_node_name){
+			dest_index = i;
+		}
+	}
+	if (dest_index == -1){
+		return backward_route; 
+	}
+//-------------------------actually starting the algorithm--------------------------
+	//for each node that is not the src node, set them to be undiscovered
+	for(int i = 0; i < graph.size(); i++){
+		if(i != src_index){
+			graph[i].discovered = 0; 
+			//Reminder: 0=undiscovered(ie first visit), 1=node visited but not adjacent nodes, 2=fully discovered 
+			graph[i].hops = 100;
+			graph[i].previous_hop = nullptr;
+		}
+	}
+	//using index of src to initialize the src node variables
+	graph[src_index].discovered = 1;
+	graph[src_index].hops = 0;
+	graph[src_index].previous_hop = nullptr;
+
+	//this is a queue to keep track of nodes that have not been fully discovered
+	queue<Node*> Q_needs_discovering;
+	Node* src_ptr = &graph[src_idex];
+	Q_needs_discovering.push(src_ptr);
+	//this is just a temporary pointer
+	Node* curr_node = nullptr; 
+	while(!Q_needs_discovering.empty()){
+		//pointer to the top node in the Queue
+		curr_node = Q_needs_discovering.front();
+
+		for(int i = 0; i < curr_node->adjlist.size(); i++){ //*** ADJLIST IS IN PRIVATE *****
+			//pointer to the current adjacent node
+			Node* adj_node = curr_node->adjlist[i];
+			if(adj_node->discovered == 0){ //if the adjacent node is undiscovered
+				adj_node->discovered = 1;
+				adj_node->hops = curr_node->hops + 1;
+				adj_node->previous_hop = Q_needs_discovering.front();
+				Q_needs_discovering.push(curr_node->adjlist[i]);
+				adj_node = nullptr; 
+			}
+		}
+		//we have accessed every adjacent node thus this current node is fully discovered
+		curr_node->discovered = 2;
+		Q_needs_discovering.pop();
+	}
+	src_ptr = nullptr;
+	curr_node = nullptr;
+
+	//:::::::::::::::: above we find the route, below we prepare the output :::::::::::::::://
+
+	curr_node = &graph[dest_index]; //starting from the destination we're working backwards
+	//reminder: { vector<string> shortest_route } the vector that holds the route backwards
+	vector<string> forward_route; //the vector that will be returned
+	backward.push_back(graph[dest_index].getName()); //push the destination in
+
+	//Im checking if destination is even reachable from the source; 
+	//100 is a placeholder for infinity
+	if(curr_node->hops < 100){ 
+		while(curr_node->previous_hop != nullptr){
+			//push in the name of the node preceding this one
+    		backward_route.push_back(curr_node->previous_hop->getName());
+    		curr_node = curr_node->previous_hop;
+		}
+		//the vector was populated backwards so I'm just swapping indices
+		int length = backward_route.size();
+		for(int i = 0; i < length; i++){
+			forward_route.push_back(shortest_route.back());
+			backward_route.pop_back();
+		}
+	}
+
+	//output the result being returned
+	cout<<"The shortestest route accoding to BFS: \n";
+	for(int i = 0; i < forawrd_route.size(); i++){
+		cout<<forawrd_route[i]<<"\t";
+	}
+	curr_node = nullptr;
+
+
+	return forward_route;
+}
+
 int main(){
 
 	
@@ -113,11 +219,39 @@ int main(){
 	vector<Hop> allhops;	
  	vector<Request> requests = receiver.requests;
 	cout << "	number of requests are" << requests.size() << endl;
+	
+	bool the_first_route = true; //we are going to service the first request
 	for (size_t r = 0; r < requests.size(); r++){
 
 		string curReqSrcId = requests.at(r).first;
 		string curReqDestId = requests.at(r).second;
 
+		if(the_first_route){
+		//----------------------------------- the BFS -------------------------------------------//
+		/* The BFS uses the graph, src node and dest node
+			the graph is basically the the vector of all nodes with each node containing a 
+			list of adjacent nodes 
+			
+			I am assuming: 
+			Graph: allnodes (list of all nodes, with allocated adjlists)
+			source name: curReqSrcId 
+			destination name: curReqDestId
+		*/
+
+		//records the shortest route
+		vector<string> shortest_route_vector = BFS(&allnodes, curReqSrcId, curReqDestId);
+		//we have just serviced the first route
+		the_first_route = false;
+		//push shortest route into the solution vector (?????)
+		everyhop.push_back(shortest_route_vector);		
+		Sender sender("routes.txt");
+		sender.export_data(everyhop);
+
+		//---------------------------------------------------------------------------------------//
+		}
+
+		else {/* ... the other stuff underneath for servicing non-first requests, idk how this works*/}
+		
 		Node srcnode;
 		Node destnode;
 
